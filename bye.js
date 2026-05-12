8dc0d783e96d5ff4e993cc05b1199c4239ee6bf4
서 여경 <syg925@naver.com>
2026-05-12 19:14:45 +0900

    수정

--- bye.js
+++
@@ -1,8 +0,0 @@
-function greet(name) {
-    console.log(`잘가, ${name}님!`);
-}
-
-const userName = "팀원";
-greet(userName);
-
-console.log("Git 테스트 중입니다! 🚀");
--- hello.js
+++
@@ -1,9 +0,0 @@
-// hello.js
-function greet(name) {
-    console.log(`안녕하세요, ${name}님!`);
-}
-
-const userName = "팀원";
-greet(userName);
-
-console.log("Git 테스트 중입니다! 🚀");
