dd606d74c4526a757b6d3b65aae896cb1be98641
JimmyApron <syg925@naver.com>
2026-06-21 10:09:14 +0900

    render port

--- backend/server.js
+++ backend/server.js
@@ -68,6 +68,8 @@ app.get('/api/rooms/:roomId/locations', (req, res) => {
   })
 })
 
-app.listen(5000, () => {
-  console.log('서버 실행 완료: http://localhost:5000')
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`서버 실행 완료: http://localhost:${PORT}`)
 })
